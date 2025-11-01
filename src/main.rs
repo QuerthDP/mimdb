63dda5281b32bbe4c2954af8976db6ca65f2ad3f
Dawid Pawlik <dawid.pawlik@scylladb.com>
2025-11-02 00:01:36 +0100

    Add Cargo init template

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025-present Dawid Pawlik
+ *
+ * For educational use only by employees and students of MIMUW.
+ * See LICENSE file for details.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
